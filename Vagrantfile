92aa38ae19097629f0cd4ef273357b60df40c5e2
André Guimarães <andreguimaster@gmail.com>
2019-03-27 23:00:14 +0000

    Add Vagrantfile and puppet manifest
    Vagrantfile and initial configurations for a java dev virtual machine.

---
+++ Vagrantfile
@@ -0,0 +1,24 @@
+Vagrant.configure("2") do |config|
+	config.vm.box = "hashicorp/precise64"
+	
+	config.vm.define :java do |java_config|
+		java_config.vm.network "forwarded_port", guest: 8080, host: 8081
+		java_config.vm.provision "shell", inline: "sudo apt-get update && sudo apt-get install -y puppet"
+		
+		java_config.vm.provision "puppet" do |puppet|
+			puppet.manifest_file = "java.pp"
+		end
+
+		java_config.vm.provision :reload
+	end
+
+	# Habilita interface gráfica, configura memória, define número de cpus e configura o proxy
+	config.vm.provider :virtualbox do |vb|
+        vb.gui = true
+        vb.memory = 4096
+        vb.cpus = 3
+
+		vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    	vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+    end
+end
---
+++ manifests/java.pp
@@ -0,0 +1,41 @@
+package { ["software-properties-common", "python-software-properties"]:
+    ensure => installed
+}
+
+exec { "add-java-repo":
+	command => "sudo add-apt-repository -y ppa:openjdk-r/ppa",
+	path => "/usr/bin",
+	require => Package["software-properties-common", "python-software-properties"]
+}
+
+exec { "apt-key-update":
+	command => "sudo apt-key update",
+	path => "/usr/bin",
+	require => Exec["add-java-repo"]
+}
+
+exec { "apt-update":
+	command => "sudo apt-get update",
+	path => "/usr/bin",
+	require => Exec["apt-key-update"]
+}
+
+package { ["ubuntu-desktop"]:
+    ensure => present,
+    require => Exec["apt-update"]
+}
+
+package { ["virtualbox-guest-utils","virtualbox-guest-dkms"]:
+    ensure => present,
+    require => Package["ubuntu-desktop"]
+}
+
+package { ["ubuntu-restricted-extras"]:
+    ensure => present,
+    require => Exec["apt-update"]
+}
+
+package { ["openjdk-8-jdk"]:
+    ensure => present,
+    require => Exec["apt-update"]
+}
